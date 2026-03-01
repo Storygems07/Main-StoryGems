618eda4e40c3249336460f84c0f2f9a1713001f9
Storygems07 <storygems07@gmail.com>
2026-03-01 20:26:36 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoa8B1K0lxOV54BrRweYvmjxM5YYIiBvQ",
+  authDomain: "storygems-6ac57.firebaseapp.com",
+  projectId: "storygems-6ac57",
+  storageBucket: "storygems-6ac57.firebasestorage.app",
+  messagingSenderId: "872029087113",
+  appId: "1:872029087113:web:7a9f30109379b21aaced58",
+  measurementId: "G-Y0HJE06TVN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged };
