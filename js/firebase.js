829aef6c903d4b85b11a26f16f5b701fbf2789ee
Storygems07 <storygems07@gmail.com>
2026-03-12 05:49:29 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,36 +1,45 @@
 // js/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  updateProfile
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged,
+updateProfile
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
-// Firebase configuration
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAoa8B1K0lxOV54BrRweYvmjxM5YYIiBvQ",
-  authDomain: "storygems-6ac57.firebaseapp.com",
-  projectId: "storygems-6ac57",
-  storageBucket: "storygems-6ac57.firebasestorage.app",
-  messagingSenderId: "872029087113",
-  appId: "1:872029087113:web:7a9f30109379b21aaced58",
-  measurementId: "G-Y0HJE06TVN"
+apiKey: "AIzaSyAoa8B1K0lxOV54BrRweYvmjxM5YYIiBvQ",
+authDomain: "storygems-6ac57.firebaseapp.com",
+projectId: "storygems-6ac57",
+storageBucket: "storygems-6ac57.firebasestorage.app",
+messagingSenderId: "872029087113",
+appId: "1:872029087113:web:7a9f30109379b21aaced58",
+measurementId: "G-Y0HJE06TVN"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
+
 const auth = getAuth(app);
 
+const db = getFirestore(app);
+
 export {
-  auth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  updateProfile
+auth,
+db,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged,
+updateProfile
 };
