a2e84e6d68c61cf9c82d4738584c58b86d1281e7
Storygems07 <storygems07@gmail.com>
2026-07-23 04:13:10 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -18,7 +18,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyAoa8B1K0lxOV54BrRweYvmjxM5YYIiBvQ",
   authDomain: "storygems-6ac57.firebaseapp.com",
   projectId: "storygems-6ac57",
   storageBucket: "storygems-6ac57.firebasestorage.app",
